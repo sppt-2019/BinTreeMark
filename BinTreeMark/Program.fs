d621634f2fbb1cf2677856d07125e93e87c11635
Tobias Morell <tobiasmorell@hotmail.com>
2019-02-27 14:56:47 +0100

    Change test runners to imperative style

--- BinTreeMark/Program.fs
+++ BinTreeMark/Program.fs
@@ -2,6 +2,7 @@
 open BinTreeMark.Problems.LeafAccumulator
 open BinTreeMark.Problems.Linpack
 open System
+open System.IO
 
 type OS =
         | OSX            
@@ -14,17 +15,23 @@ let getOS =
         | 6       -> OSX
         | _       -> Windows
 
+let OpenFile fileName =
+    if File.Exists(fileName) then
+        File.Delete(fileName)
+    let file = new StreamWriter(fileName)
+    file
 
 [<EntryPoint>]
 let main argv =
     printfn "Setting up benchmarks"
     let l = new Linpack ()
-    let sumProblems = List.map (fun s -> pown 2 s) [1..12]
-    let probs = [createTree 1 getRandomNumber; createTree 2 getRandomNumber; createTree 3 getRandomNumber; createTree 4 getRandomNumber; createTree 5 getRandomNumber]                                                                                                                                    getRandomNumber]
-
-    let sumSeqRunner = new MorellRunner<int list list, int, int>(l.SumSequential, sumProblems, l.Setup, "Matrix Sum Sequential", 100L)
-    let sumMaReRunner = new MorellRunner<int list list, int, int>(l.SumMapReduce, sumProblems, l.Setup, "Matrix Sum Map Reduce", 100L)
-    let sumPaRunner = new MorellRunner<int list list, int, int>(l.SumParallel, sumProblems, l.Setup, "Matrix Sum Parallel", 100L)    
+    let matrixSizes = List.map (fun b-> pown 2 b) [1..12]
+    let sumProblems = List.map (fun s -> (s, l.Setup(s))) matrixSizes
+    let probs = List.map (fun b -> (b, createTree b getRandomNumber)) [1..5]
+    
+    let sumSeqRunner = new McCollinRunner<int list list, int>(l.SumSequential, sumProblems, "Matrix Sum Sequential", 100L)
+    let sumMaReRunner = new McCollinRunner<int list list, int>(l.SumMapReduce, sumProblems, "Matrix Sum Map Reduce", 100L)
+    let sumPaRunner = new McCollinRunner<int list list, int>(l.SumParallel, sumProblems, "Matrix Sum Parallel", 100L)    
     let eagerRunner = new McCollinRunner<tree, int list>(leaves, probs, "Eager Sequential", 100L)
     let lazyRunner = new McCollinRunner<tree, int list>(lazyLeaves, probs, "Lazy Sequential", 100L)
     let eagerAsyncRunner = new McCollinRunner<tree, int list>(asyncLeaves, probs, "Eager Async", 100L)
@@ -48,19 +55,45 @@ let main argv =
     lazyTPLParallelRunner.Run()
     
     printfn ""
-    printfn "Results"
-    printfn "%s" titleFormat
-    printfn "%s" (sumSeqRunner.Result())
-    printfn "%s" (sumMaReRunner.Result())
-    printfn "%s" (sumPaRunner.Result())
-    printfn "%s" (eagerRunner.Result())
-    printfn "%s" (lazyRunner.Result())
-    printfn "%s" (eagerAsyncRunner.Result())
-    printfn "%s" (lazyAsyncRunner.Result())
-    printfn "%s" (eagerParaRunner.Result())
-    printfn "%s" (lazyParaRunner.Result())
-    printfn "%s" (tplParallelRunner.Result())
-    printfn "%s" (lazyTPLParallelRunner.Result())
+    printfn "Done, writing files"
+
+    let file = OpenFile "fsharp-linpack.csv"
+    file.WriteLine "Problem Size,Sequential,Sequential Error,Map Reduce,Map Reduce Error,Parallel,Parallel Error"
+
+    for res in sumSeqRunner.Results do
+        let ma = sumMaReRunner.Results.[res.Key]
+        let pa = sumPaRunner.Results.[res.Key]
+        let line = (sprintf "%d,%f,%f,%f,%f,%f,%f" res.Key (fst res.Value) (snd res.Value) (fst ma) (snd ma) (fst pa) (snd pa))
+
+        file.WriteLine line
+    file.Flush ()
+    file.Close ()
+
+    let file = OpenFile "fsharp-accumulate.csv"
+    file.WriteLine "Problem Size,Eager Sequential,Eager Sequential Error,Lazy Sequential,Lazy Sequential Error,Eager Async,Eager Async Error,Lazy Async,Lazy Async Error,Eager Parallel,Eager Parallel Error,Lazy Parallel,Lazy Parallel Error,Eager TPL,Eager TPL Error,Lazy TPL,Lazy TPL Error"
+
+    for eagerSeq in eagerRunner.Results do
+        let lazySeq = lazyRunner.Results.[eagerSeq.Key]
+        let eagerAsync = eagerAsyncRunner.Results.[eagerSeq.Key]
+        let lazyAsync = lazyAsyncRunner.Results.[eagerSeq.Key]
+        let eagerPar = eagerParaRunner.Results.[eagerSeq.Key]
+        let lazyPar = lazyParaRunner.Results.[eagerSeq.Key]
+        let eagerTPL = tplParallelRunner.Results.[eagerSeq.Key]
+        let lazyTPL = lazyTPLParallelRunner.Results.[eagerSeq.Key]
+        let line = (sprintf "%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f" eagerSeq.Key (fst eagerSeq.Value) (snd eagerSeq.Value) 
+            (fst lazySeq) (snd lazySeq)
+            (fst eagerAsync) (snd eagerAsync)
+            (fst lazyAsync) (snd lazyAsync)
+            (fst eagerPar) (snd eagerPar)
+            (fst lazyPar) (snd lazyPar)
+            (fst eagerTPL) (snd eagerTPL)
+            (fst lazyTPL) (snd lazyTPL))
+
+        file.WriteLine line
+    file.Flush ()
+    file.Close ()
+
+    printfn "Files be written"
     
     if getOS = Windows then
         System.Console.ReadLine()
--- BinTreeMark/TestRunners.fs
+++ BinTreeMark/TestRunners.fs
@@ -2,6 +2,7 @@ module BinTreeMark.TestRunners
 
 open System
 open BinTreeMark.Statistics
+open System.Collections.Generic
 
 type Clock() =
     let mutable start = System.Diagnostics.Stopwatch.GetTimestamp()
@@ -40,7 +41,7 @@ type SestoftRunner<'T, 'U>(func:'T -> 'U, args:'T, message, duration:TimeSpan) =
         this.ArithmeticMean <- fst resTuple
         this.Deviation <- snd resTuple
 
-type MorellRunner<'T, 'P, 'U>(func:'T -> 'U, problems:'P list, problemGenerator:'P -> 'T, message:string, repitions:int64) =
+type MorellRunner<'T, 'P, 'U>(func:'T -> 'U, problems:int list, problemGenerator:int -> 'T, message:string, repitions:int64) =
     let func = func
     let msg = message
     let probs = problems
@@ -48,70 +49,53 @@ type MorellRunner<'T, 'P, 'U>(func:'T -> 'U, problems:'P list, problemGenerator:
     let clock = new Clock()
     
     let mutable aMean : double = Double.NaN
-    let mutable results : string list = []
-    
-    member val ArithmeticMean = aMean with get, set                   
+    let mutable results : Dictionary<int, double> = new Dictionary<int, double>()
+                     
     member val Results = results with get, set
-    
-    member this.Result () = sprintf "%-30s \t %20.2f" msg this.ArithmeticMean
     
     member this.Run () =
         let mutable i = 0
+        let mutable times = []
         for prob in probs do
-            let rec RunWhile (reps:int64) (sum:double) (times:double list) (iterations:int64) =
-                if iterations < reps then
-                    let problem = problemGenerator prob 
-                    clock.Start()
-                    let res = func problem
-                    let time = double(clock.Check())
-                    let newTimes = List.append times [time]
-                    RunWhile reps (time + sum) newTimes (iterations + 1L)
-                else
-                    let m = mean sum (double(iterations))
-                    m
-            let m = RunWhile reps 0.0 [] 0L
-            this.ArithmeticMean <- m
-            this.Results <- List.append this.Results [this.Result()]
+            for j in [0L..reps] do
+                let problem = problemGenerator prob 
+                clock.Start()
+                let res = func problem
+                let time = double(clock.Check())
+                times <- List.append times [time]
+
+            let m = mean (List.sum times) (double(reps))
+            this.Results.[prob] <- m
             i <- i + 1
             printf "%s: %d/%d" msg i probs.Length
             System.Console.SetCursorPosition(0, System.Console.CursorTop)
         printfn ""
 
-type McCollinRunner<'T, 'U>(func:'T -> 'U, problems:'T list, message:string, repitions:int64) =
+type McCollinRunner<'T, 'U>(func:'T -> 'U, problems:(int * 'T) list, message:string, repitions:int64) =
     let func = func
     let msg = message
     let probs = problems
     let reps = repitions
     let clock = new Clock()
     
-    let mutable aMean : double = Double.NaN
-    let mutable sDeviation : double = Double.NaN
-    let mutable results : string list = []
+    let mutable results : Dictionary<int, (double * double)> = new Dictionary<int, (double*double)>()
     
-    member val ArithmeticMean = aMean with get, set                   
-    member val Deviation = sDeviation with get, set
     member val Results = results with get, set
-    
-    member this.Result () = sprintf "%-30s \t %20.2f \t %20.2f" msg this.ArithmeticMean this.Deviation
     
     member this.Run () =
         let mutable i = 0
         for problem in probs do
-            let rec RunWhile (reps:int64) (sum:double) (times:double list) (iterations:int64) =
-                if iterations < reps then
-                    clock.Start()
-                    let res = func problem
-                    let time = double(clock.Check())
-                    let newTimes = List.append times [time]
-                    RunWhile reps (time + sum) newTimes (iterations + 1L)
-                else
-                    let m = mean sum (double(iterations))
-                    let sd = standardDeviation times m iterations
-                    (m, sd)
-            let resTuple = RunWhile reps 0.0 [] 0L
-            this.ArithmeticMean <- fst resTuple
-            this.Deviation <- snd resTuple
-            this.Results <- List.append this.Results [this.Result()]
+            let mutable times = []
+            for j in [0L..reps] do
+                clock.Start()
+                let res = func (snd problem)
+                let time = double(clock.Check())
+                times <- List.append times [time]
+
+            let m = mean (List.sum times) (double(reps))
+            let sd = standardDeviation times m reps
+            let resTuple = (m, sd)
+            this.Results.[fst problem] <- resTuple
             i <- i + 1
             printf "%s: %d/%d" msg i probs.Length
             System.Console.SetCursorPosition(0, System.Console.CursorTop)
